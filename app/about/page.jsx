6430fd5a6551e31d58375f54aa550724190b4649
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-08-25 19:51:45 -0300

    Primer proyecto Next

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,8 +2,7 @@
 export default function page() {
   return (
     <div>
-        <h4>Hola yo, todos los que nos tratan de segunda opcion</h4>
-        <h1>En cualquier momento nos van haber muy arriba y ahi van a tenerme en cuenta <br/> Pero va hacer muy tarde para ellos</h1>
+        <h4>Alimenta tu mente con sueños, nutre tu corazón con pasión y deja que la motivación te guíe hacia un mañana lleno de posibilidades infinitas.</h4>
     </div>
   )
 }
